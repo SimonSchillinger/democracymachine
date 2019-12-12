6ea04c4cd4de6dc60316bb52f22beebee9345b8e
Simon Schillinger <simon.schillinger@web.de>
2019-12-12 11:16:26 +0100

    using not instead if if else

--- clean_questions.py
+++ clean_questions.py
@@ -1,18 +1,19 @@
-
+import sys
 import csv
+
+#file_name = "english_clean.csv"
+cq_file = open(file_name, "w+")
 with open('english.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='\n')
         for row in csv_reader:
             question = ''.join(row)
-            if question[0].isalpha(): 
-                question = question
-            else:
-                q_start = 0
-                for char in range(0,5):
-                    if question[char].isalpha():
-                        dosth=2
-                    else:
-                       q_start = q_start+1  
-                question = question[q_start:len(question)]
-            print(question)
+            
+            q_start = 0
+            for char_pos in range(0,5):
+                if not question[char_pos].isalpha():
+                    q_start = q_start+1  
+                else:
+                    break
+            question = question[q_start:len(question)]
+            cq_file.write(question + "\n")
 
