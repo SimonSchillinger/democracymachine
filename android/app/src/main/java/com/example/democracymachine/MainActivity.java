7e7fb34fa8474298ab5784d254c30bcdd0d5a7dd
SimonSchillinger <simon.schillinger92.ss@gmail.com>
2019-12-15 16:58:16 +0100

    app is working and Button can get set up now

--- android/app/src/main/java/com/example/democracymachine/MainActivity.java
+++ android/app/src/main/java/com/example/democracymachine/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.democracymachine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    BluetoothAdapter mBluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Items initialization
+        Button btnFindRaspi     = (Button) findViewById(R.id.findRaspi);
+        Button btnStartMachine  = (Button) findViewById(R.id.startMachine);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        //ClickListener
+        btnFindRaspi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMsg("find Raspi");
+                findRaspi();
+            }
+        });
+
+        btnStartMachine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "start Machine",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
+    // Functions
+    public void toastMsg(String msg) {
+
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+    public void findRaspi() {
+
+
+    }
+
 }
+
+
